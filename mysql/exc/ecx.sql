350ce1c772e5cfeb3b5f0ccf45caca612d9bf574
yym2044 <yym2044@gmail.com>
2022-01-24 11:52:38 +0900

    .

---
+++ mysql/exc/ecx.sql
@@ -0,0 +1,19 @@
+use world;
+
+show tables;
+
+-- 주석 
+
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
